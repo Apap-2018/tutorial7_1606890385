e9e04c66bea88dc857912a2feedcc5b8be783b44
shafira21 <fitrishaff21@gmail.com>
2018-10-31 20:38:40 +0700

    add year with library

--- src/main/java/com/apap/tutorial7/controller/ModelController.java
+++ src/main/java/com/apap/tutorial7/controller/ModelController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+
+
 import com.apap.tutorial7.rest.Setting;
 
 @RestController
@@ -29,11 +32,21 @@ public class ModelController {
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.add("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/69.0.3497.100 Safari/537.36");
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		String path = Setting.modelUrl + "/?cmd=getModels&make="+ factory+"&year=2018";
-		ResponseEntity<String> res =restTemplate.exchange(path,HttpMethod.GET, entity, String.class);
-		//Object response = restTemplate.exchange(path,HttpMethod.GET, entity, Object.class);
-		return res;
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		String path = Setting.modelUrl +"/?cmd=getModels&make="+ factory+"&year="+year;
+		//String path = Setting.modelUrl + "/?cmd=getModels&make="+ factory+"&year=2018";
+		ResponseEntity<String> response = restTemplate.exchange(path,HttpMethod.GET, entity, String.class);
+		return response;
 	}
+	
+/*	@GetMapping(value = "/full")
+	private ModelDetail postStatus(@RequestParam ("factory") String factory) throws Exception{
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		String path = Setting.modelUrl +"/?cmd=getModels&make="+ factory+"&year="+year;
+		
+		Object response = restTemplate.exchange(path,HttpMethod.GET, entity, Object.class);
+		return response;
+	}*/
 	
 	
 
